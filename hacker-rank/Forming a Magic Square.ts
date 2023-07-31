ad8cdd79c3e43d09553b7c3bf142c3c966950d33
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-01 02:25:10 +0300

    feat: solve problem

---
+++ hacker-rank/Forming a Magic Square.ts
@@ -0,0 +1,70 @@
+// Problem Link: https://www.hackerrank.com/challenges/magic-square-forming
+
+function formingMagicSquare(s: number[][]): number {
+  const possibleMagicSquares = [
+    [
+      [8, 1, 6],
+      [3, 5, 7],
+      [4, 9, 2],
+    ],
+    [
+      [6, 1, 8],
+      [7, 5, 3],
+      [2, 9, 4],
+    ],
+    [
+      [4, 9, 2],
+      [3, 5, 7],
+      [8, 1, 6],
+    ],
+    [
+      [2, 9, 4],
+      [7, 5, 3],
+      [6, 1, 8],
+    ],
+    [
+      [8, 3, 4],
+      [1, 5, 9],
+      [6, 7, 2],
+    ],
+    [
+      [4, 3, 8],
+      [9, 5, 1],
+      [2, 7, 6],
+    ],
+    [
+      [6, 7, 2],
+      [1, 5, 9],
+      [8, 3, 4],
+    ],
+    [
+      [2, 7, 6],
+      [9, 5, 1],
+      [4, 3, 8],
+    ],
+  ];
+
+  let minCost = Infinity;
+
+  possibleMagicSquares.forEach((possibleMagicSquare) => {
+    let cost = 0;
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        cost += Math.abs(s[i][j] - possibleMagicSquare[i][j]);
+      }
+    }
+
+    minCost = Math.min(minCost, cost);
+  });
+
+  return minCost;
+}
+
+console.log(
+  formingMagicSquare([
+    [5, 3, 4],
+    [1, 5, 8],
+    [6, 4, 2],
+  ])
+);
