5e958c246c7a9831c74cb3e1a08ca4bcc79edfad
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-03 03:24:30 +0300

    feat: solve a problem

---
+++ leet-code/102. Binary Tree Level Order Traversal.ts
@@ -0,0 +1,56 @@
+// Problem Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const queue: TreeNode[] = [root];
+
+  while (queue.length) {
+    const level: number[] = [];
+
+    const initialQueueLength = queue.length;
+
+    for (let count = 0; count < initialQueueLength; count++) {
+      const node = queue.shift() as TreeNode;
+
+      level.push(node.val);
+
+      if (node.left) {
+        queue.push(node.left as TreeNode);
+      }
+
+      if (node.right) {
+        queue.push(node.right as TreeNode);
+      }
+    }
+
+    result.push(level);
+  }
+
+  return result;
+}
+
+const node7 = new TreeNode(7);
+const node15 = new TreeNode(15);
+const node20 = new TreeNode(20, node15, node7);
+const node9 = new TreeNode(9);
+const node3 = new TreeNode(3, node9, node20);
+
+console.log(levelOrder(node3));
