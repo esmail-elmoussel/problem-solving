c8ddde6d87e1dbbde393655dd65da35f45122c19
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-11 04:03:14 +0300

    feat: solve a problem

---
+++ leet-code/1211. Queries Quality and Percentage.sql
@@ -0,0 +1,6 @@
+SELECT 
+query_name, 
+ROUND(AVG(rating / position), 2) AS quality,
+ROUND(COUNT(CASE WHEN rating < 3 THEN 1 END) / COUNT(*) * 100, 2) AS poor_query_percentage
+FROM queries q 
+GROUP BY query_name
