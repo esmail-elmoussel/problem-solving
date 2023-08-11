4c4aecaa1b7d205f596e077aef0a96f374743c99
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-11 03:56:15 +0300

    feat: solve a problem

---
+++ leet-code/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,6 @@
+-- Problem Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+
+SELECT customer_id, COUNT(*) AS count_no_trans FROM Visits 
+LEFT JOIN Transactions ON Transactions.visit_id = Visits.visit_id
+WHERE Transactions.transaction_id IS NULL
+GROUP BY customer_id
