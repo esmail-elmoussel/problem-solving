6f88f9c70dc6f89b3fc9f4e447f1d898d40e38e7
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-11 04:03:17 +0300

    feat: solve a problem

---
+++ leet-code/1251. Average Selling Price.sql
@@ -0,0 +1,8 @@
+SELECT 
+u.product_id, 
+ROUND(SUM(p.price * u.units) / SUM(u.units), 2) AS average_price
+FROM prices p 
+JOIN unitssold u 
+ON u.product_id = p.product_id 
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY u.product_id
