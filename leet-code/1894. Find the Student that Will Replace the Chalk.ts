425e5a652971107212b63e367c8b10683cdb999e
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-12 03:01:04 +0300

    feat: solve a problem

---
+++ leet-code/1894. Find the Student that Will Replace the Chalk.ts
@@ -0,0 +1,29 @@
+// Problem Link: https://leetcode.com/problems/two-sum/
+
+export function chalkReplacer(chalk: number[], k: number): number {
+  let chalksCount = 0;
+
+  for (let i = 0; i < chalk.length; i++) {
+    chalksCount += chalk[i];
+  }
+
+  // Just to reduce (optimize) number of iterations
+  k = k % chalksCount;
+
+  let i = 0;
+
+  while (true) {
+    if (chalk[i] > k) {
+      return i;
+    }
+
+    k -= chalk[i];
+
+    i++;
+  }
+}
+
+const chalk = [5, 1, 5],
+  k = 22;
+
+console.log(chalkReplacer(chalk, k));
