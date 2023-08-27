043384ff836a38e89458648ddcc78e5a24fd4475
Esmail Elmoussel <elmoussel12@gmail.com>
2023-08-27 10:18:36 +0300

    feat: add tree helpers

--- leet-code/98. Validate Binary Search Tree.ts
+++ leet-code/98. Validate Binary Search Tree.ts
@@ -1,18 +1,6 @@
 // Problem Link: https://leetcode.com/problems/validate-binary-search-tree/
 
-/**
- * Definition for a binary tree node.
- */
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+import { TreeNode } from "./helpers/tree";
 
 export function isValidBST(root: TreeNode | null): boolean {
   const validateNode = (
