bbc2c867756e6d09ebf67f49c81bd9c28ee573e3
Esmail Elmoussel <elmoussel12@gmail.com>
2024-12-24 13:51:54 +0200

    refactor: improve solution

--- leet-code/1744. Can You Eat Your Favorite Candy on Your Favorite Day.ts
+++ leet-code/1744. Can You Eat Your Favorite Candy on Your Favorite Day.ts
@@ -1,12 +1,10 @@
 // Problem Link: https://leetcode.com/problems/can-you-eat-your-favorite-candy-on-your-favorite-day/
 
 export function canEat(candiesCount: number[], queries: number[][]): boolean[] {
-  const minCandies: number[] = [1];
-  const maxCandies: number[] = [candiesCount[0]];
+  const prefixSum: number[] = [0];
 
   for (let i = 1; i < candiesCount.length; i++) {
-    minCandies.push(candiesCount[i - 1] + minCandies[i - 1]);
-    maxCandies.push(candiesCount[i] + maxCandies[i - 1]);
+    prefixSum.push(candiesCount[i - 1] + prefixSum[i - 1]);
   }
 
   const result: boolean[] = [];
@@ -14,13 +12,17 @@ export function canEat(candiesCount: number[], queries: number[][]): boolean[] {
   for (let i = 0; i < queries.length; i++) {
     const [favoriteType, favoriteDay, dailyCap] = queries[i];
 
-    if (minCandies[favoriteType] > (favoriteDay + 1) * dailyCap) {
+    const minCandies: number = prefixSum[favoriteType] + 1;
+    const maxCandies: number =
+      prefixSum[favoriteType] + candiesCount[favoriteType];
+
+    if (minCandies > (favoriteDay + 1) * dailyCap) {
       result.push(false);
 
       continue;
     }
 
-    if (maxCandies[favoriteType] < favoriteDay + 1) {
+    if (maxCandies < favoriteDay + 1) {
       result.push(false);
 
       continue;
@@ -44,7 +46,8 @@ const candiesCount = [5, 2, 6, 4, 1],
 
 console.log(canEat(candiesCount, queries));
 
-// candies = [    5,    2,    6,    4,      1]
-// min     = [    1,    6,    8,   14,     18]
-// max     = [    5,    7,   13,   17,     18]
-// result  = [false, true, true, false, false]
+// candies   = [    5,    2,    6,    4,      1]
+// prefixSum = [    0,    5,    7,   13,     17]
+// min       = [    1,    6,    8,   14,     18]
+// max       = [    5,    7,   13,   17,     18]
+// result    = [false, true, true, false, false]
